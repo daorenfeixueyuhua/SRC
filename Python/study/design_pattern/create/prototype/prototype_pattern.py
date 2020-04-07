df812f0373f372b7f4973b49964c0397a0bf17e7
daoren <daorenfeixueyuhua@163.com>
2020-04-07 12:01:27 +0800

    新增： 设计模式： 工厂模式、抽象工厂模式，单例模式，原型模式

---
+++ Python/study/design_pattern/create/prototype/prototype_pattern.py
@@ -0,0 +1,12 @@
+class Shape:
+    __id__ = ''
+    type = ''
+
+    def draw(self):
+        pass
+
+    def get_id(self):
+        return self.__id__
+
+    def get_type(self):
+        return self.type
