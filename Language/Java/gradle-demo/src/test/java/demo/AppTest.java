47fb7784f564687d875ae743f431ae7969edf42c
daoren <daorenfeixueyuhua@163.com>
2020-04-17 22:16:44 +0800

    commit

--- Language/Java/gradle-demo/src/test/java/demo/AppTest.java
+++ Language/Java/gradle-demo/src/test/java/demo/AppTest.java
@@ -1,10 +1,11 @@
 package demo;
-import org.junit.Test;
-import static org.junit.Assert.*;
 public class AppTest{
-    @Test
+    
     public void testappHasACreeting(){
         App classUnderTest = new App();
         assertNotNull("app should hava a greeting", classUnderTest.getString());
+    }
+
+    private void assertNotNull(String string, String string2) {
     }
 }
