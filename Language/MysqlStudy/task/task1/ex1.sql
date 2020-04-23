fe966c42fb47f4597cba262598e7312e4904c724
daoren <daorenfeixueyuhua@163.com>
2020-04-23 17:47:43 +0800

    Mysql第一次作业

---
+++ Language/MysqlStudy/task/task1/ex1.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS db_mr DEFAULT CHARSET utf8 COLLATE utf8_general_ci;
+
+show databases;
+
+show databases like 'db%';
+
+DROP DATABASE IF EXISTS db_mr;
+
+show databases;
