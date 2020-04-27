8a3242d481715fccc39877c346477a7aadecff35
daoren <daorenfeixueyuhua@163.com>
2020-04-27 19:28:22 +0800

    删除设计模式内容

--- Language/Python/study/design_pattern/example/factory.py
+++
@@ -1,8 +0,0 @@
-from study.design_pattern.create.singleton.singleton_pattern import Singleton
-
-
-class Factory(Singleton):
-    def get_bean(self, obj):
-        pass
-
-
--- Language/Python/study/design_pattern/one_day_code/day01/RabbitAndChicken.py
+++
@@ -1,4 +0,0 @@
-for rabbit in range(0, 31):
-  for chicken in range(0, 31):
-    if rabbit+chicken == 30 and rabbit*4 + chicken*2==88:
-      print("rabbit:", rabbit, "chicken:", chicken)
